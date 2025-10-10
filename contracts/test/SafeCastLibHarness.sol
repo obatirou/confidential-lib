aa3557f42ff17c1fbd1017d63458650c64fa3c33
Oba <obatirou@gmail.com>
2025-10-10 13:31:35 +0200

    Adds SafeCastLibHarness.sol

---
+++ contracts/test/SafeCastLibHarness.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/// @notice Stub import to ensure the SafeCast harness is compiled.
+// solhint-disable-next-line no-unused-import
+import {SafeCastLibHarness} from "../../test/helpers/SafeCastLibHarness.sol";
